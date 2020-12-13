fixing normalization border error

snntoolbox/conversion/utils.py
@@ -893,9 +893,9 @@ def channel_norm_J(model, config, norm_set=None, num_samples=None, divisions=1, 
             continue
 
         # Scale parameters
-        parameters = [np.array(w, dtype='float32') for w in layer.get_weights()]
-        lbdas = np.array(scale_facs[layer.name][0], dtype='float32')
-        shifts = np.array(scale_facs[layer.name][1], dtype='float32')
+        parameters = [np.array(tf.convert_to_tensor(w), dtype='float64') for w in layer.get_weights()]
+        lbdas = np.array(scale_facs[layer.name][0], dtype='float64')
+        shifts = np.array(scale_facs[layer.name][1], dtype='float64')
         #denom = lbdas-shifts
         denom = np.array([aux if aux!=0 else 1 for aux in [l-s for l,s in zip(lbdas,shifts)]], dtype='float64')
         inbound = get_inbound_layers_with_params(layer)
@@ -909,22 +909,30 @@ def channel_norm_J(model, config, norm_set=None, num_samples=None, divisions=1, 
                 (parameters[1]-shifts)/denom
                 ]
         elif len(inbound) == 1:
-            lbdas0  = np.array(scale_facs[inbound[0].name][0], dtype='float32')
-            shifts0 = np.array(scale_facs[inbound[0].name][1], dtype='float32')
+            lbdas0  = np.array(scale_facs[inbound[0].name][0], dtype='float64')
+            shifts0 = np.array(scale_facs[inbound[0].name][1], dtype='float64')
+            denom0 = np.array([aux if aux!=0 else 1 for aux in [l-s for l,s in zip(lbdas0,shifts0)]], dtype='float64')
             parameters_norm = parameters.copy()
-            for i in range(len(lbdas0)):
-                den0 = (lbdas0[i]-shifts0[i])
-                den0 = den0 if den0!=0 else 1
-                parameters_norm[1] += np.sum(parameters[0][:,:,i],(0,1))*shifts0[i]
-                parameters_norm[0][:,:,i] = parameters[0][:,:,i]*den0/denom
-            parameters_norm[1] = (parameters_norm[1]-shifts)/denom
+
+            if layer.__class__.__name__ == 'NormConv2D':
+                parameters_norm[0] = parameters[0]/denom
+                parameters_norm[1] = (parameters[1]-shifts)/denom
+                parameters_norm[2] = denom0
+                parameters_norm[3] = shifts0
+            else:
+                for i in range(len(lbdas0)):
+                    den0 = (lbdas0[i]-shifts0[i])
+                    den0 = den0 if den0!=0 else 1
+                    parameters_norm[1] += np.sum(parameters[0][:,:,i],(0,1))*shifts0[i]
+                    parameters_norm[0][:,:,i] = parameters[0][:,:,i]*den0/denom
+                parameters_norm[1] = (parameters_norm[1]-shifts)/denom
 
         else:
             parameters_norm = parameters.copy()
             offset = 0  # Index offset at input filter dimension
             for i,inb in enumerate(inbound):
-                lbdas0  = np.array(scale_facs[inb.name][0], dtype='float32')
-                shifts0 = np.array(scale_facs[inb.name][1], dtype='float32')
+                lbdas0  = np.array(scale_facs[inb.name][0], dtype='float64')
+                shifts0 = np.array(scale_facs[inb.name][1], dtype='float64')
                 f_out = len(lbdas0)  # Num output features of inbound layer
                 unshift = np.zeros(f_out)
                 for j in range(f_out):

snntoolbox/simulation/backends/custom_layers.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import keras
 from keras import backend as K
-from keras.layers import Layer
+from keras.layers import Layer, Conv2D
 from tensorflow.python.keras import activations
 
 class NormAdd(Layer):
@@ -145,3 +145,49 @@ class NormReshape(Layer):
           except ValueError:
                print('Weights need to be of shape: [',tf.shape(self.lmbda),',',tf.shape(self.shift),'].')
 
+
+class NormConv2D(Conv2D):
+     def __init__(self, **kwargs):
+          self.initial_weights=kwargs.pop('weights', None)               
+          super(NormConv2D, self).__init__(**kwargs) 
+
+     def build(self, input_shape):
+          super(NormConv2D, self).build(input_shape)
+          # Normalization
+          self.norm = [self.add_weight(
+                         name="denom",
+                         shape = (input_shape[-1],),
+                         initializer = "ones", trainable = False
+                         ),
+                    self.add_weight(
+                         name="shift",
+                         shape = (input_shape[-1],),
+                         initializer = "zeros", trainable = False
+                         )
+               ]
+          if self.initial_weights is not None:
+               self.set_weights(self.initial_weights)
+               self._initial_weights = self.initial_weights
+               del self.initial_weights
+
+     def call(self, x):
+          x = x*self.norm[0] + self.norm[1]
+          return Conv2D.call(self, x)
+
+     def set_weights(self,weights):
+          if len(weights)==4:
+               w = [tf.convert_to_tensor(u) for u in weights]
+               super().set_weights(w)
+          elif len(weights)==2:
+               extra = tf.convert_to_tensor(self.norm)
+               super().set_weights(list(weights)+[extra[0]]+[extra[1]])
+          else:
+               raise ValueError(
+               'You called `set_weights(weights)` on layer "%s" '
+               'with a weight list of length %s, but the layer was '
+               'expecting 4 weights. Provided weights: %s...' %
+               (self.name, len(weights), str(weights)[:50]))
+
+     def get_weights(self):
+          extra = tf.convert_to_tensor(self.norm)
+          return [self.kernel, self.bias, extra[0], extra[1]]